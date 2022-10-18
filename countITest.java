63a737f02d1622d18dfb9e5418bffe714a480b32
MadamGisele92 <112910766+MadamGisele92@users.noreply.github.com>
2022-10-18 19:40:19 -0400

    Update countITest.java

--- countITest.java
+++ countITest.java
@@ -9,8 +9,8 @@ class countITest {
 	@Test
 	void test() {
 		WordCount test = new WordCount();
-		int result = test.countI("C:\\Users\\gbrown62\\eclipse-workspace\\WordCount\\src\\Testing\\TheRaven.txt");
-		assertEquals(32, result);
+		int result = test.countI("Once upon a midnight dreary, while I pondered, weak and weary,Over many a quaint and curious volume of forgotten lore— While I nodded, nearly napping, suddenly there came a tapping,	As of some one gently rapping, rapping at my chamber door. Tis some visiter I muttered, tapping at my chamber door— Only this and nothing more. Ah, distinctly I remember it was in the bleak December, And each separate dying ember wrought its ghost upon the floor. Eagerly I wished the morrow;—vainly I had sought to borrow 	From my books surcease of sorrow—sorrow for the lost Lenore— For the rare and radiant maiden whom the angels name Lenore- Nameless here for evermore.");
+		assertEquals(6, result);
 	}
 
 }
